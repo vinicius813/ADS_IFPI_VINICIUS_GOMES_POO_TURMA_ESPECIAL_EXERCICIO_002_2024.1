cc905d5fb07b004dac11e89da5d36a562ab34343
Vinícius Costa <138374162+vinicius813@users.noreply.github.com>
2024-03-03 12:53:33 -0300

    Add files via upload

---
+++ interpolacao_de_strings.js
@@ -0,0 +1,5 @@
+var professor = 'Ely';
+var Tempopagamento = 120.56;
+var linguagem = 'TypeScript';
+var mensagem = "\n".concat(professor, "\nMy payment time is ").concat(Tempopagamento, "\nand\nmy preffered language is ").concat(linguagem, "\n");
+console.log(mensagem);
---
+++ interpolacao_de_strings.ts
@@ -0,0 +1,16 @@
+const professor : string = 'Ely'
+const Tempopagamento : number = 120.56
+const linguagem : string = 'TypeScript'
+
+const mensagem : string = `
+${professor}
+My payment time is ${Tempopagamento}
+and
+my preffered language is ${linguagem}
+`;
+
+console.log(mensagem);
+
+    
+
+       
